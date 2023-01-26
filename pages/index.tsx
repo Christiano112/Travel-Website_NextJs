5dd91b24b2b14f4499137aabb85e1aec6d54c911
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-01-26 22:15:26 +0100

    started the challenge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,60 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.card}>
-            <Image
-              src="/images/menu-6.jpg"
-              alt="image"
-              width={400}
-              height={300}
-              priority
-            />
-          </div>
+      <main>
+        <h1 className='text-3xl text-center text-primary-500'>Hello World</h1>
       </main>
     </>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#212832",
+          200: "#181E4B",
+          300: "#5E6282",
+          400: "#14183E",
+          500: "#1E1D4C",
+          600: "#080809",
+          700: "#84829A",
+        },
+        secondary: {
+          100: "#DF6951",
+          200: "#F1A501"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
