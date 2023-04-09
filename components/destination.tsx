3c31d483f7681de26cf8d4477656a4424cca9968
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-04-09 14:31:03 +0100

    fixed responsiveness issues

--- components/destination.tsx
+++ components/destination.tsx
@@ -31,8 +31,8 @@ const Destination = () => {
             <h2 className='h2'>Top Destinations</h2>
             <div className='flex items-center justify-evenly gap-8 mt-[2rem] md:mt-[4rem] flex-wrap lg:flex-nowrap'>
                 {destinationContent.map((destination, index) => (
-                    <div key={index} className='flex flex-col rounded-2xl pb-4 shadow-2xl z-10'>
-                        <Image src={destination.img} alt='destinations' width='400' height='600' className='md:min-w-[25rem] w-full justify-self-center mb-[-5rem] md:ml-[-4rem] md:mr-[-8.6rem]' />
+                    <div key={index} className='flex flex-col rounded-2xl pb-4 shadow-2xl z-10 min-w-min max-w-max'>
+                        <Image src={destination.img} alt='destinations' width='400' height='600' className='lg:min-w-[25rem] w-full justify-self-center mb-[-5rem] lg:ml-[-4rem] lg:mr-[-8.6rem]' />
                         <div className='flex justify-between px-4 mb-4'>
                             <p className='text-primary-300 font-medium text-lg'>{destination.location}</p>
                             <p className='text-primary-300 font-medium text-lg'>{destination.amount}</p>
